6c7b259402713304bde90075fbe8737283608ae8
subodh kant <subodhkantdnr@gmail.com>
2023-12-09 14:04:55 +0530

    Initial commit

---
+++ .vscode/function.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+//area of circle by function
+// float printarea(float a, int r){
+//     a=3.14*r*r;
+//     return a;
+// }
+
+
+// int main(){
+//     float a;
+//     int r;
+//     cout<<"enter r";
+//     cin>>r;
+//     float area = printarea(a,r);
+//     cout<<area;
+//     return 0;
+// }
+
+
+//even or odd by function
+// void evenodd(int n){
+//     if(n%2==0){
+//         cout<<"even is"<<n;
+//     }
+//     else{
+//         cout<<"odd is"<<n;
+//     }
+// }
+
+
+// int main(){
+//     int n;
+//     cout<<"ente the n";
+//     cin>>n;
+//     evenodd(n);
+//     return 0;
+//}
+
+
+//factorial number by function
+// void printfact(int n){
+//     int fact = 1;
+//     for(int i =1; i<=n; i++){
+//         fact = fact * i;
+//         
+        
+//     }
+//      cout<<fact;
+// }
+
+
+
+// int main(){
+//     int n;
+//     cout<<"enterr the number";
+//     cin>>n;
+//      printfact(n);
+//     return 0;
+// }
+
+
+
+//prime or not by function
+// void getprime(int n){
+//     int count =0;
+//     for(int i = 1; i <= n; i++){
+//         if(n%i==0){
+//         count++;
+//         }
+//     }
+//     if(count==2){
+//         cout<<"prime number"<<n;
+//     }
+//     else{
+//         cout<<"not prime number"<<n;
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"enter the number";
+//     cin>>n;
+//     getprime(n);
+//     return 0;
+// }
+
+
